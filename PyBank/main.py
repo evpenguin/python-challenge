c9a787351c0385374b8b7a7a6c626af71f41c260
evpenguin <evangeline.rowney@gmail.com>
2023-06-16 18:02:57 +1000

    code comments added

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,7 +7,7 @@ import os
 csvpath = os.path.join("Resources", "budget_data.csv")
 with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-#initialize variables
+    #initialize variables
     header = next(csvreader)
     num_months = 0
     total_profit = 0.0
@@ -20,28 +20,30 @@ with open(csvpath, 'r') as csvfile:
     x = 0.0
 #loop through each row and do the calculations
     for row in csvreader:
+        #total profit up to row
         total_profit += float(row[1])
+        #add profit/loss change to list
         delta.append(float(row[1])-x)
+        #update storage variable 
         x = float(row[1])
+        #check if it's the greatest profit, if not check if it's the greatest loss
         if delta[num_months] > greatest_profit:
             greatest_profit = delta[num_months]
-            #this month thing could be a bit off
             greatest_profit_month = row[0]
         elif delta[num_months] < greatest_loss:
             greatest_loss = delta[num_months]
-            #this month thing could be a bit off
             greatest_loss_month = row[0]
+        #track number of months - this is last in the loop because this doubles as the index for delta
         num_months += 1
 
 #calculate average of profit change
-#i feel like this calculation will need work
-
 y = 0
 for x in range(85):
     a = delta[x+1]
     y = y + a
     average_change = y/(num_months-1)
 
+#output to terminal
 print("Financial Analysis")
 print("----------------------------")
 print("Total Months: " + str(num_months))
@@ -50,6 +52,7 @@ print("Average Change: $" + str(round(average_change, 2)))
 print("Greatest Increase in Profits: " + greatest_profit_month + " (" + str(greatest_profit) + ")")
 print("Greatest Decrease in Profits: " + greatest_loss_month + " (" + str(greatest_loss) + ")")
 
+#output to .txt file
 output_path = os.path.join("analysis", "analysis.txt")
 with open(output_path, 'w') as output:
     output.write("Financial Analysis\n----------------------------\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,15 +7,17 @@ import os
 csvpath = os.path.join("Resources", "election_data.csv")
 with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-#initialize variables
+    #initialize variables
     header = next(csvreader)
     total_votes = 0
-    #how do I store this info using a combo of lists/dictionaries 
     votes_calc = {}
 
+    #loop through the cvs and tally votes
     for row in csvreader:
+        #if the candidate is already in the dictionary, add one to their tally
         if row[2] in votes_calc:
             votes_calc[row[2]] += 1
+        #if not, add the candidate to the dictionary and start tally
         else:
             votes_calc.update({row[2]: 1})
         total_votes += 1
@@ -26,13 +28,15 @@ winner = ""
 highest_votes = 0
 votes_percentage = {}
 for candidate in votes_calc:
+    #calculate votes percentage
     percentage = (votes_calc[candidate]/total_votes) * 100
     votes_percentage.update({candidate: round(percentage,3)})
+    #if they're winning, make them the winner
     if votes_calc[candidate] > highest_votes:
         highest_votes = votes_calc[candidate]
         winner = candidate
 
-
+#output to terminal
 print("Election Results")
 print("-------------------------")
 print("Total Votes: " + str(total_votes))
